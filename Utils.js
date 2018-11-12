2dfc225e041a5d930448819641193eb9473377a5
Kim Bogyun <bogyun@elicer.com>
2018-11-12 10:03:33 +0900

    custom for elice mobile

--- Utils.js
+++ Utils.js
@@ -1,54 +1,55 @@
 class Utils {
+  // Was causing crashes with RN > 0.55, removed for now.
+  //
+  // static isTextOnly(nodes) {
+  //     if (nodes.length) {
+  //         for (let i = 0; i < nodes.length; i++) {
+  //             if (nodes[i] &&
+  //                 nodes[i].hasOwnProperty('type') &&
+  //                 nodes[i].type.hasOwnProperty('displayName')) {
+  //                 if (nodes[i].type.displayName !== 'Text') {
+  //                     return false;
+  //                 }
+  //             }
 
-    // Was causing crashes with RN > 0.55, removed for now.
-    // 
-    // static isTextOnly(nodes) {
-    //     if (nodes.length) {
-    //         for (let i = 0; i < nodes.length; i++) {
-    //             if (nodes[i] &&
-    //                 nodes[i].hasOwnProperty('type') &&
-    //                 nodes[i].type.hasOwnProperty('displayName')) {
-    //                 if (nodes[i].type.displayName !== 'Text') {
-    //                     return false;
-    //                 }
-    //             }
+  //         }
+  //     }
+  //     return true;
+  // }
 
-    //         }
-    //     }
-    //     return true;
-    // }
+  static concatStyles (extras, newStyle) {
+    let newExtras
+    if (extras) {
+      newExtras = JSON.parse(JSON.stringify(extras))
 
-    static concatStyles(extras, newStyle) {
-        let newExtras;
-        if (extras) {
-            newExtras = JSON.parse(JSON.stringify(extras));
-
-            if (extras.style) {
-                newExtras.style.push(newStyle);
-            } else {
-                newExtras.style = [newStyle];
-            }
-        } else {
-            newExtras = {
-                style: [newStyle]
-            };
-        }
-        return newExtras;
+      if (extras.style) {
+        newExtras.style.push(newStyle)
+      } else {
+        newExtras.style = [newStyle]
+      }
+    } else {
+      newExtras = {
+        style: [newStyle]
+      }
     }
+    return newExtras
+  }
 
-    static logDebug(nodeTree, level = 0) {
-        for (let i = 0; i < nodeTree.length; i++) {
-            const node = nodeTree[i];
+  static logDebug (nodeTree, level = 0) {
+    for (let i = 0; i < nodeTree.length; i++) {
+      const node = nodeTree[i]
 
-            if (node) {
-                let prefix = Array(level).join('-');
-                console.log(prefix + '> ' + node.key + ', NODE TYPE: ' + node.type.displayName);
-                if (Array.isArray(node.props.children)) {
-                    this.logDebug(node.props.children, level + 1);
-                }
-            }
+      if (node) {
+        let prefix = Array(level).join('-')
+        console.log(
+          prefix + '> ' + node.key + ', NODE TYPE: ' + node.type.displayName
+        )
+        if (Array.isArray(node.props.children)) {
+          this.logDebug(node.props.children, level + 1)
         }
+      }
     }
+  }
 }
 
-module.exports = Utils;
+module.exports = Utils
--- styles.js
+++ styles.js
@@ -1,107 +1,112 @@
 const defaultStyles = {
-    block: {
-        marginBottom: 10,
-        flexWrap: 'wrap',
-        flexDirection: 'row'
-    },
-    blockQuote: {
-        borderLeftWidth: 5,
-        borderLeftColor: '#aaaaaa',
-        backgroundColor: '#cccccc',
-        paddingLeft: 10
-    },
-    h1: {
-        fontSize: 30,
-        marginTop: 20,
-        marginBottom: 8
-    },
-    h2: {
-        fontSize: 20,
-        marginTop: 16,
-        marginBottom: 8
-    },
-    h3: {
-        fontSize: 20,
-        marginTop: 16,
-        marginBottom: 8
-    },
-    h4: {
-        fontSize: 20,
-        marginTop: 16,
-        marginBottom: 8
-    },
-    h5: {
-        fontSize: 20,
-        marginTop: 12,
-        marginBottom: 6
-    },
-    h6: {
-        fontSize: 20,
-        marginTop: 12,
-        marginBottom: 6
-    },
-    hr: {
-        alignSelf: 'stretch',
-        height: 1,
-        backgroundColor: '#333333',
-        marginVertical: 8,
-    },
-    text: {
-        alignSelf: 'flex-start'
-    },
-    strong: {
-        fontWeight: 'bold',
-    },
-    em: {
-        fontStyle: 'italic',
-    },
-    del: {
-        textDecorationLine: 'line-through',
-    },
-    u: {
-        textDecorationLine: 'underline',
-    },
-    linkWrapper: {
-        alignSelf: 'flex-start',
-    },
-    link: {
-        textDecorationLine: 'underline',
-        alignSelf: 'flex-start'
-    },
-    list: {
-        marginBottom: 20
-    },
-    listItem: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginVertical: 5,
-    },
-    listItemContent: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-    },
-    listItemBullet: {
-        width: 4,
-        height: 4,
-        backgroundColor: 'black',
-        borderRadius: 2,
-        marginRight: 10,
-    },
-    listItemNumber: {
-        marginRight: 10
-    },
-    imageWrapper: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-start'
-    },
-    image: {
-        flex: 1,
-        minWidth: 200,
-        height: 200
-    }
-};
+  block: {
+    marginBottom: 10,
+    flexWrap: 'wrap',
+    flexDirection: 'row'
+  },
+  blockQuote: {
+    borderLeftWidth: 5,
+    borderLeftColor: '#aaaaaa',
+    backgroundColor: '#cccccc',
+    paddingLeft: 10
+  },
+  blockItem: {
+    backgroundColor: '#222222',
+    color: 'blue',
+    fontSize: 90
+  },
+  h1: {
+    fontSize: 30,
+    marginTop: 20,
+    marginBottom: 8
+  },
+  h2: {
+    fontSize: 20,
+    marginTop: 16,
+    marginBottom: 8
+  },
+  h3: {
+    fontSize: 20,
+    marginTop: 16,
+    marginBottom: 8
+  },
+  h4: {
+    fontSize: 20,
+    marginTop: 16,
+    marginBottom: 8
+  },
+  h5: {
+    fontSize: 20,
+    marginTop: 12,
+    marginBottom: 6
+  },
+  h6: {
+    fontSize: 20,
+    marginTop: 12,
+    marginBottom: 6
+  },
+  hr: {
+    alignSelf: 'stretch',
+    height: 1,
+    backgroundColor: '#333333',
+    marginVertical: 8
+  },
+  text: {
+    alignSelf: 'flex-start'
+  },
+  strong: {
+    fontWeight: 'bold'
+  },
+  em: {
+    fontStyle: 'italic'
+  },
+  del: {
+    textDecorationLine: 'line-through'
+  },
+  u: {
+    textDecorationLine: 'underline'
+  },
+  linkWrapper: {
+    alignSelf: 'flex-start'
+  },
+  link: {
+    textDecorationLine: 'underline',
+    alignSelf: 'flex-start'
+  },
+  list: {
+    marginBottom: 20
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginVertical: 5
+  },
+  listItemContent: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  listItemBullet: {
+    width: 4,
+    height: 4,
+    backgroundColor: 'black',
+    borderRadius: 2,
+    marginRight: 10
+  },
+  listItemNumber: {
+    marginRight: 10
+  },
+  imageWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+  image: {
+    flex: 1,
+    minWidth: 200,
+    height: 200
+  }
+}
 
-module.exports = defaultStyles;
+module.exports = defaultStyles
